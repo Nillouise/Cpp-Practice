f9bcf0e55c0162618403c5a9c0200380a2e499b8
Nillouise <LovelyNihilism@outlook.com>
2018-02-27 22:24:30 +0800

    Primer C++ Section 7 class/7.58.cpp

---
+++ Primer C++ Section 7 class/7.58.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+
+class Example{
+public:
+    //这里应该是错的，不能再类内给非const或constexpr的静态成员赋值
+    static double rate = 6.5;
+    //这个应该是对的，但不能被其他地方是用
+    static const int vecSize = 20;
+    //这个应该是对的，因为vecSize是常量
+    static vector<double> vec(vecSize);
+};
+
+double Example::rate;
+vertor<double> Example::vec;
+
+int main()
+{
+    //freopen("I:\\Algorithms\\git\\Algorithm\\Algorithm\\input.txt","r",stdin);//
+    
+    ios::sync_with_stdio(false);
+    
+    
+    
+    
+    return 0;
+}
