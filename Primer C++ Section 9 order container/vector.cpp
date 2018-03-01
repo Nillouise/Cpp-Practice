60879ae0e012882a7e3af168f117bd03268a3717
Nillouise <LovelyNihilism@outlook.com>
2018-03-01 22:46:50 +0800

    container

---
+++ Primer C++ Section 9 order container/vector.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+
+
+
+
+int main()
+{
+    //freopen("I:\\Algorithms\\git\\Algorithm\\Algorithm\\input.txt","r",stdin);//
+    
+    ios::sync_with_stdio(false);
+    
+    vector<string> vec{10};
+    cout<<vec.size();
+    
+    
+    return 0;
+}
