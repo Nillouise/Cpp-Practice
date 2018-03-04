1da88db6bd1f6dbeb0dddbe91304c2dea3043357
Nillouise <LovelyNihilism@outlook.com>
2018-03-04 18:58:43 +0800

    function decltype

---
+++ Primer C++ Section 6 Function/6.54.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+
+using fun1 = int(*)(int,int);
+using fun2 = decltype(fun1);
+
+typedef int fun3(int,int);
+typedef int (*fun4)(int,int);
+
+
+
+int main()
+{
+    //freopen("I:\\Algorithms\\git\\Algorithm\\Algorithm\\input.txt","r",stdin);//
+    
+    ios::sync_with_stdio(false);
+    vector<fun>vec;
+    
+    
+    
+    return 0;
+}
