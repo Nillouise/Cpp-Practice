0db61489a0eb34c8d45862bf652c2fbad209398c
Nillouise <LovelyNihilism@outlook.com>
2018-03-11 19:49:48 +0800

    namespace and overload

---
+++ Primer C++ Section 17-19/18.20.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+
+
+namespace primerLib{
+    void compute();
+    void compute(const void *);
+}
+using primerLib::compute;
+
+void compute(int);
+void compute(double,double = 3.4);
+void compute(char*,char* = 0);
+
+void f()
+{
+    compute(0);
+}
+
+
+
+int main()
+{
+    freopen("I:\\Algorithms\\git\\Algorithm\\Algorithm\\input.txt","r",stdin);//
+    
+    ios::sync_with_stdio(false);
+    
+    
+    
+    
+    return 0;
+}
