4f56caefe7bfd4c665a5bc360f1a0814a5699b12
Nillouise <LovelyNihilism@outlook.com>
2018-03-24 17:58:26 +0800

    enum

---
+++ Primer C++ Section 17-19/enum.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+
+enum key{
+    a=100,
+};
+
+
+int main()
+{
+    //freopen("I:\\Project\\acm\\input.txt","r",stdin);//
+    
+    ios::sync_with_stdio(false);
+    
+    map<string,key> m;
+    // m["oo"]=0;
+    cout<<m["oo"]<<endl;
+    
+    
+    return 0;
+}
