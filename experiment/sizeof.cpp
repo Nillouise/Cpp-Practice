98f66c51aa26b9e31b66562a2f43995c705e6461
Nillouise <LovelyNihilism@outlook.com>
2018-04-10 23:06:15 +0800

    virtual size

---
+++ experiment/sizeof.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+
+struct node1
+{
+    int a;
+    char b;
+        short c;
+}S1;
+typedef struct node4
+{
+    bool a;
+    node1 s1;
+    short b;
+}S4;
+
+int main()
+{
+    //freopen("I:\\Project\\acm\\Algorithm\\input.txt","r",stdin);//
+    
+    ios::sync_with_stdio(false);
+    cout<<sizeof(S1)<<endl;//
+    cout<<sizeof(S4)<<endl;
+    
+    
+    return 0;
+}
