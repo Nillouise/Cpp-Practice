6c9ff08d421f029b673d82a4098d64258bb3934f
Nillouise <LovelyNihilism@outlook.com>
2018-07-15 14:46:57 +0800

    CSAPP

---
+++ CSAPP/ex 2.53.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+
+#define POS_INFINITY 1e400
+#define NEG_INFINITY (-POS_INFINITY)
+#define NEG_ZERO (-1.0/POS_INFINITY)
+
+
+int main(int argc, char const *argv[])
+{
+    printf("%lf\n", POS_INFINITY);
+    printf("%lf\n",NEG_INFINITY);
+    printf("%lf\n",NEG_ZERO);
+
+    return 0;
+}
